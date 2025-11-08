c99d566f2f338830fd7f3db16e5fae418efb385f
Avichal Kumar <Avichalllkumar@gmail.com>
2025-11-08 17:17:16 +0530

    done setup microservices application

--- config-server/src/main/java/com/davil/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/davil/config_server/ConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.davil.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 
 public class ConfigServerApplication {
 
--- service-discovery/src/main/java/com/davil/service_discovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/davil/service_discovery/ServiceDiscoveryApplication.java
@@ -2,12 +2,18 @@ package com.davil.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
+
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	
 	}
 
 }
